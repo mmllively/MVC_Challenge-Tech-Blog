a61d2381a617c662b9ed14308e87d53dc14ee915
Maggie Lively <maggielively@MaggieLvelysPro.lan>
2022-11-08 20:31:27 -0500

    added login.js, logout.js, models index.js and login.handlebars based off of previous lessons with similar examples

--- models/index.js
+++ models/index.js
@@ -1,3 +1,22 @@
 // Post will belongto one user
 //Post will havemany comments
 //Comment belongto one user
+
+const User = require('./User');
+const Gallery = require('./Comment');
+const Painting = require('./Post');
+
+Post.hasMany(Comment, {
+    foreignKey: 'comment_id',
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
+  Comment.belongsto(User, {
+    foreignKey: 'user_id',
+  });
+  
+  module.exports = { User, Gallery, Painting };
+  
